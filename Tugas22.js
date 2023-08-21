dce632bb2caf7924601786e80a3414aa109ba7a2
felster527 <91239878+felster527@users.noreply.github.com>
2023-08-21 19:40:56 +0700

    Add files via upload

---
+++ Tugas22.js
@@ -0,0 +1,13 @@
+function panggilData(x,y) {
+    var konversi = x.split(y)
+
+    konversi.forEach(function(item, index){
+        console.log("Item : ",item,"Index ke",index)
+        
+    })
+    
+}
+
+var data = "Saya ingin belajar bersama"
+
+panggilData(data," ")
